227524829f061a8dc35052e765656bc933e21de7
Tiago <tiagoportilho52@gmail.com>
2024-07-22 19:40:45 -0300

    Dice function

--- tingbot.py
+++ tingbot.py
@@ -1,6 +1,9 @@
 import discord
+import random
 
 discord_api_key = "MTI2NDk2NDQyOTE1NDQ4NDMwNw.GmsHP3.yKZx1wk_0edZ_Ptb6TVjKbAoVx8UycGRnhHWoQ"
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -9,9 +12,13 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith("$"):
+        if message.content.startswith("$ola"):
             await message.reply("Eu sou tingbot", mention_author=True)
 
+        if message.content.startswith("$dado"):
+            parts = message.content.split()
+            dmax = int(parts[1])
+            await message.reply(random.randint(1, dmax), mention_author=True)
 
 
 intents = discord.Intents.default()

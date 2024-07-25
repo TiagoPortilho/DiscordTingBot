398c1d64abd1896cb6d38b1edf520ebc5058991d
Tiago <tiagoportilho52@gmail.com>
2024-07-25 01:52:03 -0300

    Help command

--- tingbot.py
+++ tingbot.py
@@ -24,8 +24,8 @@ async def on_ready():
 
 
 @bot.command(name="ola")
-async def on_message(ctx):
-    await ctx.reply("Eu sou tingbot", mention_author=True)
+async def ola(ctx):
+    await ctx.reply("Olá, eu sou tingbot", mention_author=True)
 
 
 @bot.command(name="dado")
@@ -73,5 +73,56 @@ async def jokenpo(ctx):
 
         else:
             await ctx.send("Selecione uma opção válida.")
+
+
+class MyHelp(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return '%s%s %s' % (self.context.clean_prefix, command.qualified_name, command.signature)
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", color=discord.Color.green())
+        for cog, cmmds in mapping.items():
+            filtered = await self.filter_commands(cmmds, sort=True)
+            if command_signatures := [
+                self.get_command_signature(c) for c in filtered
+            ]:
+                cog_name = getattr(cog, "qualified_name", "Comandos")
+                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command), color=discord.Color.green())
+        if command.help:
+            embed.description = command.help
+        if alias := command.aliases:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_help_embed(self, title, dscrpt, cmmds):
+        embed = discord.Embed(title=title, description=dscrpt or "No help found...")
+
+        if filtered_commands := await self.filter_commands(cmmds):
+            for command in filtered_commands:
+                embed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
+
+        await self.get_destination().send(embed=embed)
+
+    async def send_cog_help(self, cog):
+        title = cog.qualified_name or "No"
+        await self.send_help_embed(f'{title} Category', cog.description, cog.get_commands())
+
+    class MyHelp(commands.HelpCommand):
+        async def send_error_message(self, error):
+            embed = discord.Embed(title="Error", description=error, color=discord.Color.red())
+            channel = self.get_destination()
+
+            await channel.send(embed=embed)
+
+
+bot.help_command = MyHelp()
 
 bot.run(discord_api_key)
